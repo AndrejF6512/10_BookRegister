87d0c60b838f9a86b60b8a082394ed50861cc568
Andrej Foltan <andrej.foltan@gmail.com>
2023-03-19 22:30:35 +0100

    first commit

---
+++ BookRegister/src/main/java/com/book/service/AuthorService.java
@@ -0,0 +1,26 @@
+package com.book.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Named;
+import javax.transaction.Transactional;
+
+import com.book.domain.AuthorEntity;
+
+@Named
+public class AuthorService extends BaseService<AuthorEntity> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    public AuthorService(){
+        super(AuthorEntity.class);
+    }
+    
+    @Transactional
+    public List<AuthorEntity> findAllAuthorEntities() {
+        
+        return entityManager.createQuery("SELECT o FROM Author o ", AuthorEntity.class).getResultList();
+    }
+    
+}
